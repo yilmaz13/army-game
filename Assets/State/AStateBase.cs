ddc2d4b4f8896e5ab73cbc9d0b0dc24a1ee2ada5
yilmaz13 <ahhmet13yilmaz@gmail.com>
2025-02-22 15:41:55 +0300

    Added Game State System

---
+++ Assets/State/AStateBase.cs
@@ -0,0 +1,17 @@
+namespace Army.State
+{
+    public abstract class AStateBase
+    {
+        public readonly string name;
+
+        //  CONSTRUCTORS
+        public AStateBase(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Activate();
+        public abstract void Deactivate();
+        public abstract void UpdateState();
+    }
+}
---
+++ Assets/State/IStateManager.cs
@@ -0,0 +1,13 @@
+namespace Army.State
+{
+    public interface IStateManager
+    {
+        //  MEMBERS
+        string CurrentState { get; }
+
+        //  METHODS
+        void AddStates(AStateBase stateHandler);
+        void ChangeState(string state);
+        void ChangeTransitionState(string state, string targetState);
+    }
+}
