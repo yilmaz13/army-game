b9d840ef2b011c1b85b60c077d538b382961a7a9
yilmaz13 <ahhmet13yilmaz@gmail.com>
2025-02-23 15:23:48 +0300

    Added Resources Systems

---
+++ Assets/Scripts/Resources/GameResources.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameResources", menuName = "ScriptableObjects/GameResources", order = 1)]
+
+public class GameResources : ScriptableObject
+{
+    //  MEMBERS    
+}
